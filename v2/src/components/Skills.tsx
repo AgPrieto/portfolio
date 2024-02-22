aab3343fa9c5f288ad474b9bc8e55e9edc394ded
Agustin Prieto <aguspv99@gmail.com>
2024-02-22 14:57:38 -0300

    psgres

--- v2/src/components/Skills.tsx
+++ v2/src/components/Skills.tsx
@@ -16,7 +16,7 @@ export default function Skills() {
         <div className="grid grid-cols-1 gap-8 py-8 text-center 2sm:grid-cols-2 sm:gap-12 md:grid-cols-3 xl:grid-cols-4">
           <Skill image="ndjs.png">NodeJs</Skill>
           <Skill image="JavaScript.png">JavaScript</Skill>
-          <Skill image="TypeScript.png">TypeScript</Skill>
+          <Skill image="psgres.png">PostgreSQL</Skill>
           <Skill image="xpress.png">Express</Skill>
           <Skill image="sequelize.png">Sequelize</Skill>
           <Skill image="React.png">React</Skill>
